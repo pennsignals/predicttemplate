084e55b5c2c23ae8d6fdded88c7150a3e69d3fbd
Jason Lubken <jlubken@users.noreply.github.com>
2023-05-16 11:03:05 -0400

    Fix pymsql build

---
+++ {{cookiecutter.name}}/scripts/version.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+	    echo '. ./version.sh --sha ${{ github.sha }} --ref ${{ github.ref }}'
+	    exit 0
+	;;
+        --sha)
+	    local sha="$1"
+	;;
+        --ref)
+	    local ref="$1"
+	;;
+        *)
+	    invalid_parameter "$1"
+	    exit 1
+    esac
+    shift
+    shift
+done
+
+VERSION="${ sha }"
+
+if [[ ${ ref } == refs/tags/* ]]; then
+  # Strip git ref prefix from version
+  TAG_NAME=$(echo "${ ref }" | sed -e 's,.*/\(.*\),\1,')
+  # Strip "v" prefix from tag name
+  VERSION=$(echo "${ TAG_NAME }" | sed -e 's/^v//')
+fi
