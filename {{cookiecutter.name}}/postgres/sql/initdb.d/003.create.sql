8aa43b1c31c8d58089efc498374cb72c0c5fbcc7
Jason Lubken <email = jlubken@users.noreply.github.com>
2024-09-13 17:15:26 -0400

    Fix typo

--- {{cookiecutter.name}}/postgres/sql/initdb.d/003.create.sql
+++ {{cookiecutter.name}}/postgres/sql/initdb.d/003.create.sql
@@ -7,6 +7,6 @@ create schema if not exists {{cookiecutter.name}};
 grant usage on schema {{cookiecutter.name}} to public;
 
 create user if not exists {{cookiecutter.name}} with password 'password';
-grant usage on schema {{coockiecutter.name}} to {{cookiecutter.name}};
+grant usage on schema {{cookiecutter.name}} to {{cookiecutter.name}};
 grant all privileges on all tables in schema {{cookiecutter.name}} to {{cookiecutter.name}};
 alter default privileges in schema {{cookiecutter.name}} grant all privileges on tables to {{cookiecutter.name}};
