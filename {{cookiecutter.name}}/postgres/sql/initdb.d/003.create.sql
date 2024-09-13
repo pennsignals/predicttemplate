fa79dd605cca4f447f01aa124bd1515ae9c1fae0
Jason Lubken <email = jlubken@users.noreply.github.com>
2024-09-13 17:12:04 -0400

    Update database service account privileges

--- {{cookiecutter.name}}/postgres/sql/initdb.d/003.create.sql
+++ {{cookiecutter.name}}/postgres/sql/initdb.d/003.create.sql
@@ -5,3 +5,8 @@ create extension if not exists tablefunc;
 
 create schema if not exists {{cookiecutter.name}};
 grant usage on schema {{cookiecutter.name}} to public;
+
+create user if not exists {{cookiecutter.name}} with password 'password';
+grant usage on schema {{coockiecutter.name}} to {{cookiecutter.name}};
+grant all privileges on all tables in schema {{cookiecutter.name}} to {{cookiecutter.name}};
+alter default privileges in schema {{cookiecutter.name}} grant all privileges on tables to {{cookiecutter.name}};
