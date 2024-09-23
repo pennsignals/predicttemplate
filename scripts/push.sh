80359a5e69af9057b6057bfd4125c303081f3a43
Jason Lubken <email = jlubken@users.noreply.github.com>
2024-09-23 15:03:25 -0400

    Update script usage

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,40 +1,54 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+usage() {
+  cat << EOF
+Usage: $0 [options]
+
+Options:
+  -p, --password string     GitHub password
+  -r, --repository string   GitHub organization/project
+  -u, --username string     GitHub username
+  -b, --branch string       Github branch
+                            (default: main)
+  -c, --canary              Github canary project
+                            (default: <project>canary)
+  -h, --host                GitHub branch
+                            (default: github.com)
+EOF
+}
+
 while [ $# -gt 0 ]; do
-  case "$1" in
+    case "$1" in
     -b|--branch)
       branch="$2"
-    ;;
+      shift 2
+      ;;
     -c|--canary)
       canary="$2"
-    ;;
+      shift 2
+      ;;
     -h|--host)
       host="$2"
+      shift 2
       ;;
     -r|--repository)
       repository="$2"
-    ;;
+      shift 2
+      ;;
     -p|--password)
       password="$2"
-    ;;
+      shift 2
+      ;;
     -u|--username)
       username="$2"
-    ;;
+      shift 2
+      ;;
     *)
-      echo "Invalid argument: $1"
-      echo "Required:"
-      echo "  --password"
-      echo "  --repository organization/project"
-      echo "  --username"
-      echo "Optional:"
-      echo "  --branch main"
-      echo "  --canary projectcanary"
-      echo "  --host github.com"
+      echo "Error: Invalid argument $1" >&2
+      usage
       exit 1
   esac
-  shift
-  shift
 done
 
 branch=${branch:-"main"}
